d9799a4546b5ec9a3384def417a2437ba4b2fdc9
swpease <swpease@gmail.com>
2024-03-12 15:26:12 -0700

    extracted grid building
    I think there's some more validation that could be implemented, but I also think I could validate til the cows come home.

--- R/CV.R
+++ R/CV.R
@@ -1,21 +1,13 @@
 #' Perform a grid search CV on `hot_deck_forecast`.
 #'
-#' Supply a list of values you want to try with `hot_deck_forecast`, and
-#' they will be combined via `tidyr::expand_grid`.
-#'
-#' If you only want symmetric windows, then supply an element of the grid
-#' arg named "window", and it will set "window_back" and "window_fwd" to that
-#' value / those values.
-#'
-#' If you want to pass samplers and mutators, you should use the helper
-#' function `grid_search_build_sampler_args`.
+#' Use `build_grid` to build the tibble to pass to the `grid` argument.
 #'
 #' `echo` prints progress like "Finished CV 3 of 8.".
 #'
 #' @param .data tsibble. The data. Passed via pipe.
 #' @param .datetime The datetime column of .data. Passed via pipe.
 #' @param .observation The observation column of .data. Passed via pipe.
-#' @param grid list. names correspond to `hot_deck_forecast` args.
+#' @param grid tibble. Created using `build_grid`.
 #' @param echo Print progress?
 #' @returns list, containing two items per element:
 #'   args: The arguments that the particular CV was called with.
@@ -27,18 +19,6 @@ grid_search_hot_deck_cv <- function(.data,
                                     .observation,
                                     grid,
                                     echo = TRUE) {
-  # TODO: extract grid building
-  sym_window = purrr::pluck_exists(grid, "window")
-  # list -> tibble of all combos
-  grid = tidyr::expand_grid(!!!grid)
-  # Not sure this is how I want to do this... put in a default arg in hdcv?
-  # TODO: dedupe grid?
-  if (isTRUE(sym_window)) {
-    grid = grid %>%
-      dplyr::rename(window_back = window) %>%
-      dplyr::mutate(window_fwd = window_back)
-  }
-
   i = 1
   outputs = vector(mode = "list", length = nrow(grid))
   # ref: https://stackoverflow.com/q/63075767/6074637
@@ -70,10 +50,82 @@ grid_search_hot_deck_cv <- function(.data,
 }
 
 
+#' Build a grid for grid search CV.
+#'
+#' The output tibble is for use in `grid_search_hot_deck_cv` as the `grid`
+#' argument.
+#'
+#' If you want to pass a non-scalar to the grid search, you need to wrap
+#' the argument in `list()`. e.g. `n_closest = list(30, c(rep(20,10),19:15)`
+#' or `window_args = list(build_window_args(3))`. Ergo, you *always* need
+#' to wrap any `window_args` or `sampler_args` in a list.
+#'
+#' @param times The number of simulated sample paths to produce per hot deck forecast.
+#' @param h How many days to forecast.
+#' @param n_closest vector of scalars or list of not-all-scalars.
+#' The number of closest observations to pick from
+#' per hot deck random sampling. Either scalar (length == 1) or vector
+#' of length == h.
+#' @param offset integer. Offset (in +- days) from the most recent row of .data
+#' to use as the starting point.
+#' @param window_args list of `build_window_args` outputs.
+#' @param sampler_args list of `build_sampler_args` outputs.
+#' @param train_test_split_type See `cv_hot_deck_forecast` details for details.
+#' @returns tibble. Grid of parameters for use in grid search.
+#'
+#' @export
+build_grid <- function(times = 30,
+                       h = 30,
+                       n_closest = 20,
+                       offset = rlang::zap(),
+                       window_args = list(),
+                       sampler_args = list(),
+                       train_test_split_type = rlang::zap()) {
+  # TODO: validate list args.
+  grid = list(
+    times = times,
+    h = h,
+    n_closest = n_closest,
+    offset = offset,
+    window_args = window_args,
+    sampler_args = sampler_args,
+    train_test_split_type = train_test_split_type
+  )
+  grid = grid %>% purrr::compact()  # removes empty args
+  # list -> tibble of all combos
+  grid = tidyr::expand_grid(!!!grid)
+  # TODO: dedupe grid?
+
+  # partial validation
+  list_args_names = c("window_args", "sampler_args")
+  names_to_check_vec = names(grid) %>% purrr::discard(\(x) x %in% list_args_names)
+  for (name in names_to_check_vec) {
+    validate_grid_arg_len(grid[[name]], h = h, name = name)
+  }
+
+  grid
+}
+
+#' Build grid search window argument.
+#'
+#' Use this function to build the `window_args` arg of `build_grid`.
+#'
+#' @param window_back The window_back. length == 1 or h.
+#' @param window_fwd The window_fwd. length == 1 or h.
+#' @returns list(window_back = window_back, window_fwd = window_fwd)
+#'
+#' @export
+build_window_args <- function(window_back, window_fwd = window_back) {
+    list(
+      window_back = window_back,
+      window_fwd = window_fwd
+    )
+}
+
+
 #' Build grid search sampler argument.
 #'
-#' This is a convenience function for if you want to pass
-#' sampler-mutator combinations to `grid_search_hot_deck_cv`.
+#' Use this function to build the `sampler_args` arg of `build_grid`.
 #'
 #' The `sm_name` argument is for downstream usage in performance
 #' assessments as a... name.
@@ -81,17 +133,40 @@ grid_search_hot_deck_cv <- function(.data,
 #' @param sm_name The name of your sampler-mutator pair.
 #' @param sampler The sampler.
 #' @param mutator The mutator.
-#' @returns list(list(sm_name = sm_name, sampler = sampler, mutator = mutator))
+#' @returns list(sm_name = sm_name, sampler = sampler, mutator = mutator)
+#'
 #' @export
-grid_search_build_sampler_args <- function(sm_name, sampler, mutator) {
-  # Yes, you need both lists.
+build_sampler_args <- function(sm_name, sampler, mutator) {
   list(
-    list(
-      sm_name = sm_name,
-      sampler = sampler,
-      mutator = mutator
-    )
+    sm_name = sm_name,
+    sampler = sampler,
+    mutator = mutator
   )
+}
+
+
+#' Validate potentially-length-h grid arguments.
+#'
+#' Works just like the similar validator in `hot_deck_forecast`,
+#' just catches this problem before you're halfway through your grid search.
+#'
+#' Currently this is an issue for `n_closest` and `window_{fwd, back}`.
+#'
+#' Raises an error if the argument is neither length-h nor scalar.
+#'
+#' @param grid_arg An argument for grid search.
+#' @param h The `h` grid search argument.
+#' @param name The name of the argument.
+validate_grid_arg_len <- function(grid_arg, h, name) {
+  invalid_els = grid_arg %>%
+    purrr::discard(\(x) length(x) %in% c(1, h))
+  if (length(invalid_els) != 0) {
+    stop(paste(name, "has elements that are the wrong length:\n",
+               invalid_els, "\n",
+               "They need to be either length 1 (i.e. 'scalar') (e.g. `x = 3L`)",
+               "or length h,", h),
+         call. = FALSE)
+  }
 }
 
 
--- tests/testthat/test-CV.R
+++ tests/testthat/test-CV.R
@@ -1,100 +1,3 @@
-test_that("grid search basic tests", {
-  # This just returns the args passed to the fn.
-  local_mocked_bindings(
-    cv_hot_deck_forecast = function(...) {rlang::list2(...)}
-  )
-
-  data = tibble(
-    datetime = as.Date("2021-04-02"),
-    obs = 10
-  ) %>%
-    as_tsibble(index = datetime)
-
-  ## basic test
-  grid = list(a = 3, b = "hi")
-  out = data %>%
-    grid_search_hot_deck_cv(
-      datetime,
-      obs,
-      grid = grid,
-      echo = FALSE
-    )
-
-  # TODO: How to test the quosures (args 2,3 to mock)?
-  expected_cv_out = c(list(data, 3, 4), grid)
-  expect_equal(out[[1]]$arg_list, grid)
-  expect_equal(length(out[[1]]$cv_out), 5)
-  expect_equal(out[[1]]$cv_out[[1]], data)
-  expect_equal(out[[1]]$cv_out[4:5], grid)
-})
-
-
-test_that("grid search window arg", {
-  # This just returns the args passed to the fn.
-  local_mocked_bindings(
-    cv_hot_deck_forecast = function(...) {rlang::list2(...)}
-  )
-
-  data = tibble(
-    datetime = as.Date("2021-04-02"),
-    obs = 10
-  ) %>%
-    as_tsibble(index = datetime)
-
-  ## basic test
-  grid = list(a = 3, b = 4, window = 10)
-  out = data %>%
-    grid_search_hot_deck_cv(
-      datetime,
-      obs,
-      grid = grid,
-      echo = FALSE
-    )
-  expected_grid = list(a = 3, b = 4, window_back = 10, window_fwd = 10)
-  expect_equal(out[[1]]$arg_list, expected_grid)
-  expect_equal(out[[1]]$cv_out[4:7], expected_grid)
-  expect_equal(out[[1]]$cv_out[[1]], data)
-})
-
-
-test_that("grid search sampler args", {
-  # This just returns the args passed to the fn.
-  local_mocked_bindings(
-    cv_hot_deck_forecast = function(...) {rlang::list2(...)}
-  )
-
-  data = tibble(
-    datetime = as.Date("2021-04-02"),
-    obs = 10
-  ) %>%
-    as_tsibble(index = datetime)
-
-  ## basic test
-  grid = list(a = 3, b = 4,
-              grid_search_build_sampler_args("a",
-                                             basic_hot_deck_sampler("hi", n_bins = 4),
-                                             lead_mutator))
-  out = data %>%
-    grid_search_hot_deck_cv(
-      datetime,
-      obs,
-      grid = grid,
-      echo = FALSE
-    )
-  expected_arg_list = list(a = 3,
-                           b = 4,
-                           sm_name = "a",
-                           sampler = basic_hot_deck_sampler("hi", n_bins = 4),
-                           mutator = lead_mutator)
-  expected_passed_arg_list = list(a = 3,
-                                  b = 4,
-                                  sampler = basic_hot_deck_sampler("hi", n_bins = 4),
-                                  mutator = lead_mutator)
-  expect_equal(out[[1]]$arg_list, expected_arg_list)
-  expect_equal(out[[1]]$cv_out[4:7], expected_passed_arg_list)
-  expect_equal(out[[1]]$cv_out[[1]], data)
-})
-
 test_that("grid search gridding", {
   # This just returns the args passed to the fn.
   local_mocked_bindings(
@@ -108,7 +11,19 @@ test_that("grid search gridding", {
     as_tsibble(index = datetime)
 
   ## basic test
-  grid = list(a = 1:2, b = 3)
+  grid = build_grid(
+    h = 5,
+    window_args = list(
+      build_window_args(window_back = 1:5),
+      build_window_args(window_back = 1, window_fwd = 2)
+    ),
+    sampler_args = list(build_sampler_args(
+      sm_name = "name",
+      sampler = basic_hot_deck_sampler("next_obs", n_bins = 8),
+      mutator = lead_mutator
+    ))
+  )
+
   out = data %>%
     grid_search_hot_deck_cv(
       datetime,
@@ -116,15 +31,57 @@ test_that("grid search gridding", {
       grid = grid,
       echo = FALSE
     )
-  expected_out_1 = list(a = 1, b = 3)
-  expected_out_2 = list(a = 2, b = 3)
 
-  expect_equal(out[[1]]$arg_list, expected_out_1)
-  expect_equal(out[[1]]$cv_out[4:5], expected_out_1)
+  expected_args_list_1 = list(
+    times = 30,
+    h = 5,
+    n_closest = 20,
+    window_back = 1:5,
+    window_fwd = 1:5,
+    sm_name = "name",
+    sampler = basic_hot_deck_sampler("next_obs", n_bins = 8),
+    mutator = lead_mutator
+  )
+  expected_passed_args_list_1 = list(
+    times = 30,
+    h = 5,
+    n_closest = 20,
+    window_back = 1:5,
+    window_fwd = 1:5,
+    sampler = basic_hot_deck_sampler("next_obs", n_bins = 8),
+    mutator = lead_mutator
+  )
+
+  expected_args_list_2 = list(
+    times = 30,
+    h = 5,
+    n_closest = 20,
+    window_back = 1,
+    window_fwd = 2,
+    sm_name = "name",
+    sampler = basic_hot_deck_sampler("next_obs", n_bins = 8),
+    mutator = lead_mutator
+  )
+  expected_passed_args_list_2 = list(
+    times = 30,
+    h = 5,
+    n_closest = 20,
+    window_back = 1,
+    window_fwd = 2,
+    sampler = basic_hot_deck_sampler("next_obs", n_bins = 8),
+    mutator = lead_mutator
+  )
+
+  expect_equal(out[[1]]$arg_list, expected_args_list_1)
+  expect_equal(tail(out[[1]]$cv_out, -3), expected_passed_args_list_1)
   expect_equal(out[[1]]$cv_out[[1]], data)
-  expect_equal(out[[2]]$arg_list, expected_out_2)
-  expect_equal(out[[2]]$cv_out[4:5], expected_out_2)
+  expect_equal(out[[2]]$arg_list, expected_args_list_2)
+  expect_equal(tail(out[[2]]$cv_out, -3), expected_passed_args_list_2)
   expect_equal(out[[2]]$cv_out[[1]], data)
+  # expect_equal(out[[1]]$cv_out[[1]], data)
+  # expect_equal(out[[2]]$arg_list, expected_out_2)
+  # expect_equal(out[[2]]$cv_out[4:5], expected_out_2)
+  # expect_equal(out[[2]]$cv_out[[1]], data)
 })
 
 
@@ -606,18 +563,80 @@ test_that("subtract a year", {
 })
 
 
-test_that("grid search sampler_arg fn", {
-  expected = list(
-    list(
-      sm_name = "a",
-      sampler = basic_hot_deck_sampler("hi", n_bins = 2),
-      mutator = lead_mutator
+# test_that("grid search sampler_arg fn", {
+#   expected = list(
+#     list(
+#       sm_name = "a",
+#       sampler = basic_hot_deck_sampler("hi", n_bins = 2),
+#       mutator = lead_mutator
+#     )
+#   )
+#   expect_equal(
+#     build_sampler_args("a",
+#                        basic_hot_deck_sampler("hi", n_bins = 2),
+#                        lead_mutator),
+#     expected
+#   )
+# })
+#
+#
+# test_that("grid search build_window_args fn", {
+#   expected_symm = list(
+#     window_back = 3,
+#     window_fwd = 3
+#   )
+#   expected_diff = list(
+#     window_back = 3,
+#     window_fwd = 4
+#   )
+#   expect_equal(build_window_args(window_back = 3), expected_symm)
+#   expect_equal(build_window_args(window_back = 3,
+#                                  window_fwd = 4), expected_diff)
+# })
+
+
+test_that("build grid", {
+  expected = tibble(
+    times = c(30, 30),
+    h = c(4, 4),
+    n_closest = c(20, 20),
+    window_args = list(
+      list(window_back = 20, window_fwd = 20),
+      list(window_back = 1:4, window_fwd = 11)
+    ),
+    sampler_args = list(
+      list(
+        sm_name = "sm_name",
+        sampler = basic_hot_deck_sampler("as"),
+        mutator = lead_mutator
+      ),
+      list(
+        sm_name = "sm_name",
+        sampler = basic_hot_deck_sampler("as"),
+        mutator = lead_mutator
+      )
     )
   )
-  expect_equal(
-    grid_search_build_sampler_args("a",
-                                  basic_hot_deck_sampler("hi", n_bins = 2),
-                                  lead_mutator),
-    expected
+
+  out = build_grid(
+    times = 30,
+    h = 4,
+    n_closest = 20,
+    window_args = list(build_window_args(20), build_window_args(1:4, 11)),
+    sampler_args = list(build_sampler_args(
+      sm_name = "sm_name",
+      sampler = basic_hot_deck_sampler("as"),
+      mutator = lead_mutator
+    ))
   )
+
+  expect_equal(out, expected)
+})
+
+
+test_that("build grid len error", {
+  expect_error(build_grid(times = 30,
+                          h = 4,
+                          n_closest = list(c(1:3))),
+               regexp = "n_closest has elements that are the wrong length")
 })
