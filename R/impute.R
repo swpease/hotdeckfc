9c3e1c06ab79755c615bf949aeebe4a075f20996
swpease <swpease@gmail.com>
2024-04-29 12:41:22 -0700

    imputation
    WIP

---
+++ R/impute.R
@@ -0,0 +1,217 @@
+#' Hot-deck Impute
+#'
+#' Impute using hot deck forecasting.
+#'
+#' @param .data tsibble. The data
+#' @param .observation symbol. The observations column.
+#' @param n_imputations integer. Number of imputations to perform.
+#' @inheritParams hot_deck_forecast
+#' @returns .data, augmented with `n_imputations` new columns of imputed
+#'   observations, named "imputation_{n}".
+#'
+#' @export
+impute <- function(.data,
+                   .observation,
+                   n_imputations,
+                   window_back = 20,
+                   window_fwd = 20,
+                   n_closest = 5,
+                   sampler = sample_lead("next_obs")) {
+  # TODO: validation
+  date_col = tsibble::index(.data)
+
+  na_tibble = build_na_tibble(.data, {{ .observation }})
+  casts = na_tibble %>%
+    purrr::pmap(\(na_len, forward_start_date, backward_start_date) {
+      cast(na_len, forward_start_date, backward_start_date, n_imputations)
+    }) %>%
+    purrr::reduce(\(acc, nxt) dplyr::bind_rows(acc, nxt))  # , .init = NULL
+
+  for (i in 1:n_imputations) {
+    imp = casts %>%
+      dplyr::filter(simulation_num == .env$i) %>%
+      dplyr::select(-simulation_num)
+    .data = dplyr::left_join(.data,
+                             imp,
+                             by = dplyr::join_by({{ date_col }} == datetime))
+    .data = .data %>%
+      dplyr::mutate("imputation_{i}" := dplyr::if_else(is.na({{ .observation }}),
+                                                       forecast,
+                                                       {{ .observation }})) %>%
+      dplyr::select(-forecast)
+  }
+
+  .data
+}
+
+
+#' @noRd
+cast <- function(na_len, fwd_start_date, back_start_date, n_imputations) {
+  invisible()
+}
+
+#' @noRd
+forecast_for_imputation <- function(.data,
+                                    .datetime,
+                                    .observation,
+                                    forward_start_date,
+                                    n_imputations,  # = times
+                                    na_len,  # = h
+                                    window_back = 20,
+                                    window_fwd = 20,
+                                    n_closest = 5,
+                                    sampler = sample_lead("next_obs")) {
+  .data = set_head(.data, forward_start_date)
+  fcs = hot_deck_forecast(.data,
+                          {{ .datetime }},
+                          {{ .observation }},
+                          times = n_imputations,
+                          h = na_len,
+                          window_back = window_back,
+                          window_fwd = window_fwd,
+                          n_closest = n_closest,
+                          sampler = sampler,
+                          covariate_forecasts = NULL)
+
+  fcs %>% dplyr::select(-h)  # Useless col for imputation.
+}
+
+
+#' Set head of data by date
+#'
+#' Moves all data beyond `date` to before earliest date in `.data`.
+#'
+#' This method co-opts [train_test_split()].
+#'
+#' @param .data tsibble. Your data
+#' @param date Date.
+#' @returns .data, reordered
+#'
+#' @noRd
+set_head <- function(.data, date) {
+  datetime = tsibble::index(.data)
+  min_date = .data %>%
+    dplyr::pull({{ datetime }}) %>%
+    dplyr::first()
+  # Edge case: two obses before NAs at head will yield a
+  # missing data point. Oh well.
+  tt_split = train_test_split(.data,
+                              {{ datetime }},
+                              ref_date = date,
+                              h = 1,
+                              window_fwd = 1,
+                              min_date = min_date,
+                              split_type = "leaky")
+  tt_split$train_data
+}
+
+
+#' Build NA tibble
+#'
+#' Build tibble of lengths, start dates, and end dates for missing data.
+#'
+#' For each row of the output, you can think of the `forward_start_date` (and
+#' `backward_start_date`) as equivalent to the observation you would use for
+#' the start of your forecast(/backcast), with the `na_len` equivalent to `h`.
+#' So if you had NAs from 2022/2/23-2022/2/25, you'd have:
+#'   - `na_len` == 3,
+#'   - `forward_start_date` == 2022/2/22
+#'   - `backward_start_date` == 2022/2/26
+#'
+#' @param .data tsibble. The data
+#' @param .observation symbol. The observations column.
+#'
+#' @returns tibble:
+#'   - na_len: integer. Length of NA sequence
+#'   - forward_start_date: Date. The latest date that bounds the respective
+#'     na_len from below (exclusive, so the latest actual observation's date)
+#'   - backward_start_date: Date. The earliest date that bounds the respective
+#'     na_len from above (exclusive, so the earliest actual observation's date)
+#'
+#' @noRd
+build_na_tibble <- function(.data, .observation) {
+  date_col = tsibble::index(.data)
+
+  augmented_data = .data %>%
+    dplyr::mutate(
+      lead_obs = dplyr::lead({{ .observation }}),
+      lag_obs = dplyr::lag({{ .observation }}))
+  fwd_start_dates = augmented_data %>%
+    dplyr::filter(!is.na({{ .observation }}),
+                  is.na(lead_obs)) %>%
+    dplyr::slice(1:(dplyr::n() - 1)) %>%
+    dplyr::pull(.env$date_col)
+  back_start_dates = augmented_data %>%
+    dplyr::filter(!is.na({{ .observation }}),
+                  is.na(lag_obs)) %>%
+    dplyr::slice(2:dplyr::n()) %>%
+    dplyr::pull(.env$date_col)
+
+  obses = .data %>% dplyr::pull({{ .observation }})
+  trimmed_obses = trim_nas(obses)
+  na_rle = rle(is.na(trimmed_obses))
+  na_lens = na_rle$length[na_rle$value]
+
+  tibble::tibble(
+    na_len = na_lens,
+    forward_start_date = fwd_start_dates,
+    backward_start_date = back_start_dates
+  )
+}
+
+
+#' Trim NAs
+#'
+#' Trim NAs (leading and trailing) from a vector.
+#'
+#' @param vec vector
+#' @returns vector without leading/trailing NAs
+#'
+#' @noRd
+trim_nas <- function(vec) {
+  first_val_idx = purrr::detect_index(vec, \(x) !is.na(x))
+  last_val_idx = purrr::detect_index(vec, \(x) !is.na(x), .dir = "backward")
+  vec[first_val_idx:last_val_idx]
+}
+
+
+#' [impute()] validation
+#'
+#' Validation for [impute()].
+#'
+#' Checks that:
+#'   1. The data is a `tsibble`.
+#'   2. The tsibble is not multi-keyed (i.e. multiple time serieses).
+#'   3. The data has no gaps (i.e. missing rows; NA observations okay).
+#'   4. The index is a Date.
+#'
+#' @inheritParams impute
+#'
+#' @noRd
+validate_imputation <- function(.data, .observation) {
+  if (isFALSE(tsibble::is_tsibble(data))) {
+    stop("Your data needs to be a `tsibble`.", call. = FALSE)
+  }
+
+  # keyless tsibbles have n_keys == 1
+  if (tsibble::n_keys(data) > 1) {
+    stop(paste("`impute` cannot handle multi-key tsibbles.\n",
+               "Multiple keys means multiple time series.",
+               "Examine your keys with `tsibble::key`,",
+               "and filter to one key's data."))
+  }
+
+  if (isTRUE(tsibble::has_gaps(data) %>% pull())) {
+    stop(paste("Your tsibble contains gaps.\n",
+               "Try using `tsibble::fill_gaps`."),
+         call. = FALSE)
+  }
+
+  is_date = .data %>%
+    dplyr::pull(tsibble::index(.)) %>%
+    is.Date()
+  if (isFALSE(is_date)) {
+    stop("Your tsibble's index need to be a `Date`.",
+         call. = FALSE)
+  }
+}
---
+++ tests/testthat/test-impute.R
@@ -0,0 +1,83 @@
+test_that("trim_nas", {
+  inner = c(1,NA,2,NA,3)
+  full = c(NA, NA, inner, NA, NA)
+  out1 = trim_nas(full)
+  out2 = trim_nas(inner)
+  expect_equal(out1, inner)
+  expect_equal(out2, inner)
+})
+
+test_that("build_na_tibble", {
+  data = tsibble::tsibble(
+    date = as.Date("2022-02-01") + 0:11,
+    obs = c(NA,2,3,NA,NA,6,7,NA,NA,NA,11,NA),
+    index = date
+  )
+
+  expected = tibble::tibble(
+    na_len = c(2, 3),
+    forward_start_date = c(as.Date("2022-02-03"), as.Date("2022-02-07")),
+    backward_start_date = c(as.Date("2022-02-06"), as.Date("2022-02-11"))
+  )
+  out = build_na_tibble(data, obs)
+  expect_equal(out, expected)
+})
+
+
+test_that("impute mocked cast", {
+  # NB: This only works for n_imputations <= 2.
+  local_mocked_bindings(
+    cast = function(na_len,
+                    forward_start_date,
+                    backward_start_date,
+                    n_imputations) {
+      a = tibble::tibble(
+        datetime = forward_start_date + 1:na_len,
+        forecast = 1,
+        simulation_num = 1
+      )
+      b = tibble::tibble(
+        datetime = forward_start_date + 1:na_len,
+        forecast = 2,
+        simulation_num = 2
+      )
+      bind_rows(a, b)
+    }
+  )
+
+  data = tsibble::tsibble(
+    date = as.Date("2022-02-01") + 0:11,
+    obs = c(NA,2,3,NA,NA,6,7,NA,NA,NA,11,NA),
+    index = date
+  )
+  expected = tsibble::tsibble(
+    date = as.Date("2022-02-01") + 0:11,
+    obs = c(NA,2,3,NA,NA,6,7,NA,NA,NA,11,NA),
+    imputation_1 = c(NA,2,3,1,1,6,7,1,1,1,11,NA),
+    imputation_2 = c(NA,2,3,2,2,6,7,2,2,2,11,NA),
+    index = date
+  )
+
+  out = impute(data, obs, n_imputations = 2)
+  expect_equal(out, expected)
+})
+
+
+test_that("set_head", {
+  data = tsibble::tsibble(
+    date = as.Date("2022-02-01") + 0:11,
+    obs = c(NA,2,3,NA,NA,6,7,NA,NA,NA,11,NA),
+    index = date
+  )
+
+  expected = tibble::tibble(
+    date = c(as.Date("2021-02-05") + 0:7,
+             as.Date("2022-02-01") + 0:3),
+    obs = c(NA,6,7,NA,NA,NA,11,NA,NA,2,3,NA)
+  ) %>%
+    as_tsibble(index = date) %>%
+    tsibble::fill_gaps()
+
+  out = set_head(data, "2022-02-04")
+  expect_equal(out, expected)
+})
